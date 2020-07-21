92a2fc0773651a8e563581379d6214ef47687379
CAEUser <e37f44db2937b2e33c487273d264a29359dc4138>
2020-07-21 09:30:41 +0000

    Generated microservice version 0.0.1

---
+++ db/MetadataTest6_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service MetadataTest6, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table .
+--
+CREATE TABLE caeschema. (
+  id INTEGER ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
